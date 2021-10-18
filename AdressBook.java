b14ee5d5ecf7095ac4c89a5b1aa13130a2e8fc66
Adityachess <adityadone9398@gmail.com>
2021-10-18 13:05:25 +0530

    Added:Welcome message.

---
+++ AdressBook.java
@@ -0,0 +1,16 @@
+package com.bz.adressbookp;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class AdressBook {
+
+	private static final Logger LOG = LogManager.getLogger("App.class");
+
+	public static void main(String[] args) {
+
+		LOG.info("Wecome to address Book Program");
+
+	}
+
+}
