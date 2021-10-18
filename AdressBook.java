790e3c729e1deebc7272dc8e99d08f58e6c420cc
Adityachess <adityadone9398@gmail.com>
2021-10-18 13:09:13 +0530

    Added:Adding new Contact Details.

--- AdressBook.java
+++ AdressBook.java
@@ -1,5 +1,7 @@
 package com.bz.adressbookp;
 
+import java.util.Scanner;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -9,8 +11,20 @@ public class AdressBook {
 
 	public static void main(String[] args) {
 
-		LOG.info("Wecome to address Book Program");
-
+		Scanner value = new Scanner(System.in);
+		int PhoneNumber;
+		String Address, FirstName, lastName;
+		LOG.info("Enter the first name");
+		FirstName = value.nextLine();
+		LOG.info("Enter the last name");
+		lastName = value.nextLine();
+		LOG.info("Enter the Address name");
+		Address = value.nextLine();
+		LOG.info("Enter the phone number");
+		PhoneNumber = value.nextInt();
+		LOG.info("FistName" + FirstName);
+		LOG.info("LastName" + lastName);
+		LOG.info("Address" + Address);
+		LOG.info("PhoneNumber" + PhoneNumber);
 	}
-
 }
